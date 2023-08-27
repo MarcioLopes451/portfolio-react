86124554c825edfbebbe9f7d700b1021be7b7773
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-08-27 23:48:36 +0100

    add:update

---
+++ src/components/Contact.jsx
@@ -0,0 +1,67 @@
+import {useState} from 'react';
+import Github from '../images/icon-github.svg';
+import Linkedin from '../images/icon-linkedin.svg';
+import Twitter from '../images/icon-twitter.svg';
+
+export default function Contact() {
+    const [name,setName] = useState('');
+    const [email, setEmail] = useState('');
+
+
+    const handleSubmit =(e) => {
+        e.preventDefault();
+    }
+  return (
+    <div className='contact'>
+        <div className='contact-container'>
+            <div className='contacting'>
+            <h1>Contact</h1>
+            <p>I would love to hear about your project and how I could help. 
+            Please fill in the form, and I will get back to you as soon as possible.
+            </p>
+            </div>
+            <div className='form-section'>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor='name'></label>
+                   <br />
+                   <input 
+                   type='text'
+                   id='name'
+                   value={name}
+                   onChange={(e) => setName(e.target.value)} />
+                   <br />
+                   <label htmlFor='name'></label>
+                   <br />
+                   <input 
+                   type='text'
+                   id='email'
+                   value={email}
+                   onChange={(e) => setEmail(e.target.value)} />
+                   <br />
+                   <label htmlFor='message'>
+                    <textarea
+                    id='message'
+                    name='message'
+                    rows='4'
+                    cols='40'
+                    placeholder='Message'
+                    >
+                    </textarea>
+                   </label>
+                   <br />
+                   <button onClick={handleSubmit}>Send Message</button>
+                </form>
+            </div>
+        </div>
+        <div className='footer-container'>
+        <p>Marcio Lopes</p>
+            <div className='social-media'>
+                <img src={Github} alt='github' />
+                <img src={Linkedin} alt='linkedin' />
+                <img src={Twitter} alt='twitter' />
+            </div>
+        </div>
+    </div>
+   
+  )
+}
---
+++ src/components/Projects.jsx
@@ -0,0 +1,58 @@
+import space from '../images/space-tourism.png';
+import entertainment from '../images/thumbnail-project-4-large.webp';
+import reddit from '../images/reddit-app-sc.png'
+
+export const project = [
+    {
+        projectName: 'Reddit App',
+        lang: 'HTML CSS JavaScript React',
+        image: reddit,
+        website:'https://marciolopes451.github.io/React-Reddit-App/',
+        repo: 'https://github.com/MarcioLopes451/React-Reddit-App',
+    },
+    {
+        projectName: 'Entertainment Web App',
+        lang: 'HTML CSS Javascript React',
+        image: entertainment,
+        website:'https://marciolopes451.github.io/entertainment-web-app/',
+        repo: 'https://github.com/MarcioLopes451/entertainment-web-app',
+    },
+    {
+        projectName: 'Space Tourism',
+        lang: 'HTML Tailwind Typescript React',
+        image: space,
+        website:'https://github.com/MarcioLopes451/space-tourism',
+        repo: 'https://marciolopes451.github.io/space-tourism/',
+    },
+   
+]
+
+export default function Projects() {
+  return (
+    <div className='projects'>
+        <h1>Projects</h1>
+        <div className='projects-container'>
+        {project.map((item,index) => (
+            <div
+            key={index}>
+                <div className='content'>
+                    <div className='project-image'>
+                    <img src={item.image}/>
+                    <div className='project-links'>
+                        <a href={item.website} target="_blank" rel="noreferrer">View Website</a>
+                        <a href={item.repo} target="_blank" rel="noreferrer">View Source Code</a>
+                    </div>
+                    </div>
+                    <h2>{item.projectName}</h2>
+                    <p>{item.lang}</p>
+                    <div className='mobile-tablet-links'>
+                        <a href={item.website} target="_blank" rel="noreferrer">View Website</a>
+                        <a href={item.repo} target="_blank" rel="noreferrer">View Source Code</a>
+                    </div>
+                </div>
+            </div>
+        ))}
+        </div>
+    </div>
+  )
+}
