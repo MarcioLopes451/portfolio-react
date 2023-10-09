91fa11793312a3478ec5f9e4184e60c548062f44
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-10-09 14:28:57 +0100

    add: update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,68 +1,77 @@
-import React from 'react'
-import Rings from '../images/pattern-rings.svg';
-
+import React from "react";
+import Rings from "../images/pattern-rings.svg";
 
 export default function Hero() {
   return (
     <>
-    <div className='hero'>
-        <img src={Rings} alt='rings' className='rings'/>
-        <div className='hero-container'>
-            <div className='intro'>
-                <h1>Nice to meet you! I'm <span>Marcio Lopes</span>.</h1>
-                <p>I'm a front-end developer who has a passion for building things and designing. 
-                    My goal in life is to design & build the best UI/UX & provide users the best 
-                    experience.
-                </p>
-                <a href='/'>Contact me</a>
-            </div>
+      <div className="hero">
+        <img src={Rings} alt="rings" className="rings" />
+        <div className="hero-container">
+          <div className="intro">
+            <h1>
+              Nice to meet you! I'm <span>Marcio Lopes</span>.
+            </h1>
+            <p>
+              I'm a front-end developer who has a passion for building things
+              and designing. My goal in life is to design & build the best UI/UX
+              & provide users the best experience.
+            </p>
+            <a href="/">Contact me</a>
+          </div>
         </div>
-    </div>
+      </div>
 
-    {/* Home Section*/}
+      {/* Home Section*/}
 
-    <div className='about'>
-        <div className='about-container'>
-            <h1>About me</h1>
-        <p> My interest began when i was in my 2nd year of university. I was deeply demotivated with 
-            the course i was doing (Accounting and Finance) and it was a point in life where i didn't 
-            know what i wanted to do in the future. I found out about Python and decided to self teach 
-            myself but it didn't go as far as i expected. then i bumped into The Learning People who gave 
-            me a online course on being a front-end Developer and now here I am today!
-            here are some of the skills I've learned since i started the course :
-        </p>
-        <ul className='lang-list'>
+      <div className="about">
+        <div className="about-container">
+          <h1>About me</h1>
+          <p>
+            My interest began when i was in my 2nd year of university. I was
+            deeply demotivated with the course i was doing (Accounting and
+            Finance) and it was a point in life where i didn't know what i
+            wanted to do in the future. I found out about Python and decided to
+            self teach myself but it didn't go as far as i expected. then i
+            bumped into The Learning People who gave me a online course on being
+            a front-end Developer and now here I am today! here are some of the
+            skills I've learned since i started the course :
+          </p>
+          <ul className="lang-list">
             <li>
-                <h2>HTML</h2>
-                <p>2 Years Experience</p>
+              <h2>HTML</h2>
+              <p>2 Years Experience</p>
             </li>
             <li>
-                <h2>CSS</h2>
-                <p>2 Years Experience</p>
+              <h2>CSS</h2>
+              <p>2 Years Experience</p>
             </li>
             <li>
-                <h2>Tailwind</h2>
-                <p>1 Years Experience</p>
+              <h2>Tailwind</h2>
+              <p>1 Years Experience</p>
             </li>
             <li>
-                <h2>Javascript</h2>
-                <p>2 Years Experience</p>
+              <h2>Javascript</h2>
+              <p>2 Years Experience</p>
             </li>
             <li>
-                <h2>Typescript</h2>
-                <p>1 Years Experience</p>
+              <h2>Typescript</h2>
+              <p>1 Years Experience</p>
             </li>
             <li>
-                <h2>React/Redux</h2>
-                <p>2 Years Experience</p>
+              <h2>React/Redux</h2>
+              <p>2 Years Experience</p>
             </li>
             <li>
-                <h2>Github/Git</h2>
-                <p>2 Years Experience</p>
+              <h2>Github/Git</h2>
+              <p>2 Years Experience</p>
             </li>
-        </ul>
+            <li>
+              <h2>NextJs</h2>
+              <p>1 Years Experience</p>
+            </li>
+          </ul>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,58 +1,80 @@
-import space from '../images/space-tourism.png';
-import entertainment from '../images/thumbnail-project-4-large.webp';
-import reddit from '../images/reddit-app-sc.png'
+import space from "../images/space-tourism.png";
+import entertainment from "../images/thumbnail-project-4-large.webp";
+import reddit from "../images/reddit-app-sc.png";
+import designo from "../images/designo-screenshot.png";
+import audiophile from "../images/audiophile.png";
 
-export const project = [
-    {
-        projectName: 'Reddit App',
-        lang: 'HTML CSS JavaScript React',
-        image: reddit,
-        website:'https://marciolopes451.github.io/React-Reddit-App/',
-        repo: 'https://github.com/MarcioLopes451/React-Reddit-App',
-    },
-    {
-        projectName: 'Entertainment Web App',
-        lang: 'HTML CSS Javascript React',
-        image: entertainment,
-        website:'https://marciolopes451.github.io/entertainment-web-app/',
-        repo: 'https://github.com/MarcioLopes451/entertainment-web-app',
-    },
-    {
-        projectName: 'Space Tourism',
-        lang: 'HTML Tailwind Typescript React',
-        image: space,
-        website:'https://github.com/MarcioLopes451/space-tourism',
-        repo: 'https://marciolopes451.github.io/space-tourism/',
-    },
-   
-]
+const project = [
+  {
+    projectName: "Reddit App",
+    lang: "HTML CSS JavaScript React",
+    image: reddit,
+    website: "https://marciolopes451.github.io/React-Reddit-App/",
+    repo: "https://github.com/MarcioLopes451/React-Reddit-App",
+  },
+  {
+    projectName: "Entertainment Web App",
+    lang: "HTML CSS Javascript React",
+    image: entertainment,
+    website: "https://marciolopes451.github.io/entertainment-web-app/",
+    repo: "https://github.com/MarcioLopes451/entertainment-web-app",
+  },
+  {
+    projectName: "Space Tourism",
+    lang: "HTML Tailwind Typescript React",
+    image: space,
+    website: "https://github.com/MarcioLopes451/space-tourism",
+    repo: "https://marciolopes451.github.io/space-tourism/",
+  },
+  {
+    projectName: "Designo App",
+    lang: "HTML Tailwind Typescript NextJs",
+    image: designo,
+    website: "https://designo-zeta.vercel.app",
+    repo: "https://github.com/MarcioLopes451/designo",
+  },
+  {
+    projectName: "Audiophile E-Commerce",
+    lang: "HTML Tailwind Javascript NextJs",
+    image: audiophile,
+    website: "https://audiophile-lake.vercel.app",
+    repo: "https://github.com/MarcioLopes451/audiophile",
+  },
+];
 
 export default function Projects() {
   return (
-    <div className='projects'>
-        <h1>Projects</h1>
-        <div className='projects-container'>
-        {project.map((item,index) => (
-            <div
-            key={index}>
-                <div className='content'>
-                    <div className='project-image'>
-                    <img src={item.image}/>
-                    <div className='project-links'>
-                        <a href={item.website} target="_blank" rel="noreferrer">View Website</a>
-                        <a href={item.repo} target="_blank" rel="noreferrer">View Source Code</a>
-                    </div>
-                    </div>
-                    <h2>{item.projectName}</h2>
-                    <p>{item.lang}</p>
-                    <div className='mobile-tablet-links'>
-                        <a href={item.website} target="_blank" rel="noreferrer">View Website</a>
-                        <a href={item.repo} target="_blank" rel="noreferrer">View Source Code</a>
-                    </div>
+    <div className="projects">
+      <h1>Projects</h1>
+      <div className="projects-container">
+        {project.map((item, index) => (
+          <div key={index}>
+            <div className="content">
+              <div className="project-image">
+                <img src={item.image} />
+                <div className="project-links">
+                  <a href={item.website} target="_blank" rel="noreferrer">
+                    View Website
+                  </a>
+                  <a href={item.repo} target="_blank" rel="noreferrer">
+                    View Source Code
+                  </a>
                 </div>
+              </div>
+              <h2>{item.projectName}</h2>
+              <p>{item.lang}</p>
+              <div className="mobile-tablet-links">
+                <a href={item.website} target="_blank" rel="noreferrer">
+                  View Website
+                </a>
+                <a href={item.repo} target="_blank" rel="noreferrer">
+                  View Source Code
+                </a>
+              </div>
             </div>
+          </div>
         ))}
-        </div>
+      </div>
     </div>
-  )
+  );
 }
