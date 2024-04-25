4bff3342eba4800faef14da868a37b7d40d4fc7d
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-04-25 14:43:46 +0100

    add: update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,51 +1,53 @@
-import space from "../images/space-tourism.png";
 import movie from "../images/1703092425337.jpeg";
 import reddit from "../images/reddit-app-sc.png";
 import designo from "../images/designo-screenshot.png";
 import audiophile from "../images/audiophile.png";
 import carurus from "../images/car-urus screenshot.png";
+import liftlabs from "../images/liftlabs screenshot.png";
+
 const project = [
   {
-    projectName: "Reddit App",
-    lang: "HTML CSS JavaScript React",
-    image: reddit,
-    website: "https://marciolopes451.github.io/React-Reddit-App/",
-    repo: "https://github.com/MarcioLopes451/React-Reddit-App",
-  },
-  {
-    projectName: "Movie App (Cineplex)",
-    lang: "HTML CSS Javascript React",
-    image: movie,
-    website: "https://marciolopes451.github.io/Movie-App/",
-    repo: "https://github.com/MarcioLopes451/Movie-App",
-  },
-  {
-    projectName: "Space Tourism",
-    lang: "HTML Tailwind Typescript React",
-    image: space,
-    website: "https://github.com/MarcioLopes451/space-tourism",
-    repo: "https://marciolopes451.github.io/space-tourism/",
-  },
-  {
-    projectName: "Designo App",
-    lang: "HTML Tailwind Typescript NextJs",
-    image: designo,
-    website: "https://designo-zeta.vercel.app",
-    repo: "https://github.com/MarcioLopes451/designo",
+    projectName: "Lift Labs",
+    lang: "React ReactNative Tailwind Typescript",
+    image: liftlabs,
+    website: "https://marciolopes451.github.io/LiftLabs/",
+    repo: "https://github.com/MarcioLopes451/LiftLabs",
+    mobileRepo: "https://github.com/MarcioLopes451/LiftLabs-App",
   },
   {
     projectName: "Audiophile E-Commerce",
-    lang: "HTML Tailwind Javascript NextJs",
+    lang: "NextJs Tailwind",
     image: audiophile,
     website: "https://audiophile-lake.vercel.app",
     repo: "https://github.com/MarcioLopes451/audiophile",
   },
   {
     projectName: "Car-Urus",
-    lang: "HTML CSS React Tailwind",
+    lang: "React Tailwind",
     image: carurus,
     website: "https://marciolopes451.github.io/car-urus/",
     repo: "https://github.com/MarcioLopes451/car-urus",
+  },
+  {
+    projectName: "Designo App",
+    lang: "NextJs Tailwind Typescript",
+    image: designo,
+    website: "https://designo-zeta.vercel.app",
+    repo: "https://github.com/MarcioLopes451/designo",
+  },
+  {
+    projectName: "Reddit App",
+    lang: "React Redux",
+    image: reddit,
+    website: "https://marciolopes451.github.io/React-Reddit-App/",
+    repo: "https://github.com/MarcioLopes451/React-Reddit-App",
+  },
+  {
+    projectName: "Movie App (Cineplex)",
+    lang: "React",
+    image: movie,
+    website: "https://marciolopes451.github.io/Movie-App/",
+    repo: "https://github.com/MarcioLopes451/Movie-App",
   },
 ];
 
@@ -66,6 +68,11 @@ export default function Projects() {
                   <a href={item.repo} target="_blank" rel="noreferrer">
                     View Source Code
                   </a>
+                  {item.mobileRepo ? (
+                    <a href={item.mobileRepo} target="_blank" rel="noreferrer">
+                      View Source Code for Mobile App
+                    </a>
+                  ) : null}
                 </div>
               </div>
               <h2>{item.projectName}</h2>
@@ -77,6 +84,11 @@ export default function Projects() {
                 <a href={item.repo} target="_blank" rel="noreferrer">
                   View Source Code
                 </a>
+                {item.mobileRepo ? (
+                  <a href={item.mobileRepo} target="_blank" rel="noreferrer">
+                    Mobile Source Code
+                  </a>
+                ) : null}
               </div>
             </div>
           </div>
